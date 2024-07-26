741cbed04f75f1404b1b0236c51b0d664e00fa83
Martyn Janes <martyn@obany.com>
2024-07-26 15:06:36 +0200

    Add guards

--- packages/api-auth-entity-storage-service/src/utils/passwordHelper.ts
+++ packages/api-auth-entity-storage-service/src/utils/passwordHelper.ts
@@ -1,12 +1,19 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
-import { Converter } from "@gtsc/core";
+import { Converter, Guards } from "@gtsc/core";
 import { Blake2b } from "@gtsc/crypto";
+import { nameof } from "@gtsc/nameof";
 
 /**
  * Helper class for password operations.
  */
 export class PasswordHelper {
+	/**
+	 * Runtime name for the class.
+	 * @internal
+	 */
+	private static readonly _CLASS_NAME: string = nameof<PasswordHelper>();
+
 	/**
 	 * Hash the password for the user.
 	 * @param passwordBytes The password bytes.
@@ -17,6 +24,9 @@ export class PasswordHelper {
 		passwordBytes: Uint8Array,
 		saltBytes: Uint8Array
 	): Promise<string> {
+		Guards.uint8Array(PasswordHelper._CLASS_NAME, nameof(passwordBytes), passwordBytes);
+		Guards.uint8Array(PasswordHelper._CLASS_NAME, nameof(saltBytes), saltBytes);
+
 		const combined = new Uint8Array(saltBytes.length + passwordBytes.length);
 		combined.set(saltBytes);
 		combined.set(passwordBytes, saltBytes.length);
